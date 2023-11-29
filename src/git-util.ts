1a4b52bcd8c7ac170344160b970f5a23cc06af37
Yuta Nakamura <nakamurayuta0204@gmail.com>
2023-11-30 01:17:39 +0900

    fix: git fetch before checking branch

--- src/git-util.ts
+++ src/git-util.ts
@@ -3,6 +3,7 @@ import simpleGit from "simple-git";
 const git = simpleGit();
 
 export async function fetchRemoteBranches() {
+  await git.fetch();
   const branches = await git.branch(["-r"]);
   return branches.all.map((branch) => branch.replace("origin/", ""));
 }
