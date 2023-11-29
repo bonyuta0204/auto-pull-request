0104d82ede5e0af6e0ca0f20305e1d00993b7ce6
Yuta Nakamura <nakamurayuta0204@gmail.com>
2023-11-29 23:23:23 +0900

    feat: base implementation for creating PRS

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,42 @@
 import core from "@actions/core";
+import { getOctokit } from "@actions/github";
 
-const srcBranch = core.getInput("src-branch");
+function main() {
+  const token = process.env.GITHUB_TOKEN;
+  if (!token) {
+    console.error("GITHUB_TOKEN not found");
+    return;
+  }
 
-console.log(srcBranch);
+  const octokit = getOctokit(token);
+
+  const srcBranch = core.getInput("src-branch");
+  const targetBranch = core.getInput("target-branch");
+
+  if (!srcBranch || !targetBranch) {
+    console.error("Source or target branch not specified");
+    return;
+  }
+
+  // Assuming the context of the action has repository information
+  const owner = process.env.GITHUB_REPOSITORY_OWNER || "";
+  const repo = (process.env.GITHUB_REPOSITORY || "").split("/")[1];
+
+  octokit.rest.pulls
+    .create({
+      owner,
+      repo,
+      title: `Merge changes from ${srcBranch} to ${targetBranch}`,
+      head: srcBranch,
+      base: targetBranch,
+      body: "Automatically created pull request",
+    })
+    .then((response) => {
+      console.log(`Pull request created: ${response.data.html_url}`);
+    })
+    .catch((error) => {
+      console.error(`Error creating pull request: ${error.message}`);
+    });
+}
+
+main();
