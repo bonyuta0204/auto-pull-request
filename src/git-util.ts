18d37fdb4d26dd32e75fda20e470af6bea20af41
Yuta Nakamura <nakamurayuta0204@gmail.com>
2023-11-30 22:53:43 +0900

    feat: change prettierrc

--- src/git-util.ts
+++ src/git-util.ts
@@ -1,44 +1,44 @@
-import { debug, info } from "@actions/core";
-import { readdir, access } from "fs/promises";
-import * as path from "path";
-import simpleGit from "simple-git";
+import { debug, info } from '@actions/core'
+import { readdir, access } from 'fs/promises'
+import * as path from 'path'
+import simpleGit from 'simple-git'
 
-const git = simpleGit();
+const git = simpleGit()
 
 /**
  * Fetches all commits from the remote repository
  */
 export async function fullFetch(): Promise<void> {
-  return access(path.join(".git", "shallow"))
+  return access(path.join('.git', 'shallow'))
     .then(async () => {
-      debug("Repository is shallow, fetching all commits");
-      await git.fetch(["--unshallow"]);
+      debug('Repository is shallow, fetching all commits')
+      await git.fetch(['--unshallow'])
     })
     .catch(async () => {
-      debug("Repository is complete, fetching commits");
-      await git.fetch([]);
-    });
+      debug('Repository is complete, fetching commits')
+      await git.fetch([])
+    })
 }
 
 export async function fetchRemoteBranches() {
-  await fullFetch();
-  const branches = await git.branch(["-r"]);
-  return branches.all.map((branch) => branch.replace("origin/", ""));
+  await fullFetch()
+  const branches = await git.branch(['-r'])
+  return branches.all.map((branch) => branch.replace('origin/', ''))
 }
 
 export async function hasCommitsBetween(
   srcBranch: string,
-  targetBranch: string,
+  targetBranch: string
 ) {
   const commits = await git.log({
     from: srcBranch,
     to: targetBranch,
-    symmetric: false,
-  });
+    symmetric: false
+  })
   debug(
     `Commits between ${srcBranch} and ${targetBranch}: ${JSON.stringify(
-      commits,
-    )}`,
-  );
-  return commits.total > 0;
+      commits
+    )}`
+  )
+  return commits.total > 0
 }
--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,33 @@
-import { getInput, setFailed, debug, info } from "@actions/core";
-import { getOctokit, context } from "@actions/github";
-import simpleGit from "simple-git";
-import { fetchRemoteBranches, hasCommitsBetween } from "./git-util";
+import { getInput, setFailed, debug, info } from '@actions/core'
+import { getOctokit, context } from '@actions/github'
+import simpleGit from 'simple-git'
+import { fetchRemoteBranches, hasCommitsBetween } from './git-util'
 
 async function main() {
-  const token = getInput("repo-token");
+  const token = getInput('repo-token')
 
-  const octokit = getOctokit(token);
+  const octokit = getOctokit(token)
 
-  const srcBranch = getInput("src-branch");
-  const targetBranch = getInput("target-branch");
+  const srcBranch = getInput('src-branch')
+  const targetBranch = getInput('target-branch')
 
-  const { repo, owner } = context.repo;
+  const { repo, owner } = context.repo
 
   if (!srcBranch || !targetBranch) {
-    setFailed("Source or target branch not specified");
-    return;
+    setFailed('Source or target branch not specified')
+    return
   }
 
-  const remoteBranches = await fetchRemoteBranches();
+  const remoteBranches = await fetchRemoteBranches()
 
   if (!remoteBranches.includes(srcBranch)) {
-    setFailed(`Source branch ${srcBranch} does not exist`);
-    return;
+    setFailed(`Source branch ${srcBranch} does not exist`)
+    return
   }
 
   if (!remoteBranches.includes(targetBranch)) {
-    setFailed(`Target branch ${targetBranch} does not exist`);
-    return;
+    setFailed(`Target branch ${targetBranch} does not exist`)
+    return
   }
 
   /** Checks if Pull Request already exists */
@@ -35,26 +35,26 @@ async function main() {
     const pulls = await octokit.rest.pulls.list({
       owner,
       repo,
-      state: "open",
+      state: 'open',
       head: `${owner}:${srcBranch}`,
-      base: targetBranch,
-    });
+      base: targetBranch
+    })
     if (pulls.data.length > 0) {
-      info(`Pull request already exists: ${pulls.data[0].html_url}`);
-      return;
+      info(`Pull request already exists: ${pulls.data[0].html_url}`)
+      return
     }
   } catch (error: any) {
-    setFailed(`Error checking for existing pull requests: ${error.message}`);
+    setFailed(`Error checking for existing pull requests: ${error.message}`)
   }
 
   /** Checks if there are commits between the source and target branch */
   const hasCommits = await hasCommitsBetween(
     `origin/${targetBranch}`,
-    `origin/${srcBranch}`,
-  );
+    `origin/${srcBranch}`
+  )
   if (!hasCommits) {
-    info(`No commits between ${srcBranch} and ${targetBranch}`);
-    return;
+    info(`No commits between ${srcBranch} and ${targetBranch}`)
+    return
   }
 
   const createParam: Parameters<typeof octokit.rest.pulls.create>[0] = {
@@ -63,19 +63,19 @@ async function main() {
     title: `Merge changes from ${srcBranch} to ${targetBranch}`,
     head: srcBranch,
     base: targetBranch,
-    body: "Automatically created pull request",
-  };
+    body: 'Automatically created pull request'
+  }
 
-  debug(`Creating pull request: ${JSON.stringify(createParam)}`);
+  debug(`Creating pull request: ${JSON.stringify(createParam)}`)
 
   octokit.rest.pulls
     .create(createParam)
     .then((response) => {
-      console.log(`Pull request created: ${response.data.html_url}`);
+      console.log(`Pull request created: ${response.data.html_url}`)
     })
     .catch((error) => {
-      setFailed(`Error creating pull request: ${error.message}`);
-    });
+      setFailed(`Error creating pull request: ${error.message}`)
+    })
 }
 
-main();
+main()
